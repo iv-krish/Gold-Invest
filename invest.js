2e41fb0fa4cdc8f642c97f1015f7e07a7efd54ed
iv-krish <82990963+iv-krish@users.noreply.github.com>
2023-02-05 20:13:48 +0530

    first commit

---
+++ invest.js
@@ -0,0 +1,16 @@
+const goldProducts = [
+    { name: "Gold Coin", price: 100 },
+    { name: "Gold Bar", price: 1000 },
+    { name: "Gold Necklace", price: 500 },
+  ];
+  
+  const silverProducts = [
+    { name: "Silver Coin", price: 50 },
+    { name: "Silver Bar", price: 500 },
+    { name: "Silver Earrings", price: 200 },
+  ];
+  
+  const goldProductsList = document.querySelector("#gold-products");
+  const silverProductsList = document.querySelector("#silver-products");
+  
+  goldProducts
